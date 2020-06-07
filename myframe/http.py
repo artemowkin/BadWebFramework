67e1e4e07dbbcbcddc4e83b8b00f24cd39faf3dd
artemowkin <artyom.groshev2017@gmail.com>
2020-06-07 22:09:46 +0900

    moved request status lines to status_from_code function

--- myframe/http.py
+++ myframe/http.py
@@ -5,6 +5,21 @@ from string import Template
 from myframe.settings import settings
 
 
+def status_from_code(status_code):
+    if status_code == 200:
+        return '200 OK'
+    elif status_code == 404:
+        return '404 Not Found'
+    elif status_code == 405:
+        return '405 Method Not Allowed'
+    elif status_code == 301:
+        return '301 Moved Permanently'
+    elif status_code == 307:
+        return '307 Temporary Redirect'
+    elif status_code == 308:
+        return '308 Permanent Redirect'
+
+
 class HTTPRequest:
 
     def __init__(self, environ):
@@ -29,24 +44,10 @@ class HTTPResponse:
             )
 
         self.status_code = status_code
-        self.status = self.get_response_status(status_code)
+        self.status = status_from_code(status_code)
         self.response_text = (text.encode() or
                               self.get_response_text(status_code))
         self.headers = headers or self.get_default_headers(status_code)
-
-    def get_response_status(self, status_code):
-        if status_code == 200:
-            return '200 OK'
-        elif status_code == 404:
-            return '404 Not Found'
-        elif status_code == 405:
-            return '405 Method Not Allowed'
-        elif status_code == 301:
-            return '301 Moved Permanently'
-        elif status_code == 307:
-            return '307 Temporary Redirect'
-        elif status_code == 308:
-            return '308 Permanent Redirect'
 
     def get_response_text(self, status_code):
         if status_code == 404:
